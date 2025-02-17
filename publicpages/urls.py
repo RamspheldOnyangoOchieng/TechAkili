b884894b6c89c5d5348271308521f12a390e4021
RamspheldOnyangoOchieng <ramspheldonyango@gmail.com>
2025-02-17 21:10:18 +0300

    V2

--- publicpages/urls.py
+++ publicpages/urls.py
@@ -2,9 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('about/', views.about, name='about'),
-    path('careers/', views.careers, name='careers'),
-    path('contactus/', views.contactus, name='contactus'),
-    path('courses/', views.courses, name='courses'),
+    path('', views.home, name='home'),                       # Home Page
+    path('about/', views.about, name='about'),                # About Us Page
+    path('services/', views.services, name='services'),       # Services Page
+    path('courses/', views.courses, name='courses'),          # Courses Page
+    path('partners/', views.partners, name='partners'),       # Partnerships Page
+    path('careers/', views.careers, name='careers'),          # Careers Page
+    path('contact/', views.contact, name='contact'),          # Contact Us Page
+    path('login/', views.login, name='login'),                # Login Page
+    path('register/', views.register, name='register')        # Register Page
 ]
--- publicpages/views.py
+++ publicpages/views.py
@@ -1,16 +1,27 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html')   # Template: home.html
 
 def about(request):
-    return render(request, 'about.html')
+    return render(request, 'about.html')  # Template: about.html
 
-def careers(request):
-    return render(request, 'careers.html')
-
-def contactus(request):
-    return render(request, 'contactus.html')
+def services(request):
+    return render(request, 'services.html')  # Template: services.html
 
 def courses(request):
-    return render(request, 'courses.html')
+    return render(request, 'courses.html')  # Template: courses.html
+
+def partners(request):
+    return render(request, 'partners.html')  # Template: partners.html
+
+def careers(request):
+    return render(request, 'careers.html')  # Template: careers.html
+
+def contact(request):
+    return render(request, 'contact.html')  # Template: contact.html
+def login(request):
+    return render(request,'login.html')
+
+def register(request):
+    return render(request, 'register.html')
